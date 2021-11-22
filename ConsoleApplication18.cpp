ab389f4fe4f3df8e8a8140d1eb10e1d7d8cc3ffd
Acevill <92327254+Acevill@users.noreply.github.com>
2021-11-22 21:39:05 +0300

    Laba 3.2

---
+++ ConsoleApplication18.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "russian");
+	int A[100][100];
+	int B[100][100];
+	int C[100][100];
+	int i, j, n;
+	cout << "Введите размер матрци А и В: ";
+	cin >> n;
+	cout << "Матрица А" << endl << endl;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			A[i][j] = rand() % 10;
+			cout << A[i][j] << "\t";
+
+		}
+		cout << endl << endl;
+	}
+	cout << "Матрица В" << endl << endl;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			B[i][j] = rand() % 10;
+			cout << B[i][j] << "\t";
+
+		}
+		cout << endl << endl;
+	}	
+	int icounter = 0;
+	int jcounter = n;
+	int max = A[0][0];
+	for (int k = 0; k < n; k++) {
+		for (int i1 = icounter; i1 < (icounter + 1); i1++) {
+			for (int j1 = 0; j1 < jcounter; j1++) {				
+				if (max < A[i1][j1])
+					max = A[i1][j1];
+			}
+		}
+		jcounter--;
+		icounter++;		
+	}	
+	cout << "Максимальное значение в матрице A = " << max << endl;
+	int icounter1 = n-1;
+	int jcounter1 = 0;
+	for (int z = 0; z < n; z++) {
+		for (int i2 = icounter1; i2 > (icounter1 - 1); i2--) {
+			for (int j2 = jcounter1; j2 < n; j2++) {				
+				if (max < B[i2][j2])
+					max = B[i2][j2];
+			}
+		}
+		icounter1--;
+		jcounter1++;		
+	}	
+	cout << "Максимальное значение в матрице B = " << max << endl;
+	cout << "Построим итоговую матрицу" << endl << endl;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			C[i][j] = max;
+			cout << C[i][j] << "\t";
+		}
+		cout << endl << endl;
+	}
+}
+
+
+
